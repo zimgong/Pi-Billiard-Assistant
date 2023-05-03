3dab1de0c642ef650ad4ed7d247059b07666b8b4
Zim <zimmergong@gmail.com>
2023-05-03 16:53:08 -0400

    video

--- video_detection.py
+++ video_detection.py
@@ -8,57 +8,90 @@ import cv2 as cv
 
 # cap = cv.VideoCapture('./696_720p.mp4')
 # cap = cv.VideoCapture('./716_720p.mp4')
-cap = cv.VideoCapture('./717_720p.mp4')
-
+cap = cv.VideoCapture('./769_480p.mp4')
 
 # Retake table map every 120 frames to avoid effect of hand movement
 count = 0
 
+def find_table(frame_hsv):
+	# hsv color range for blue pool table
+	lower_blue = np.array([110,50,50])
+	upper_blue = np.array([130,255,255])
+	# Mask out everything but the pool table (blue)
+	mask = cv.inRange(frame_hsv, lower_blue, upper_blue)
+	# Find the pool table contour
+	contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+	# Find the largest contour as the border of the table
+	try:
+		table = max(contours, key = cv.contourArea)
+	except:
+		print("No table found!")
+		return None
+	return cv.convexHull(table)
+
 while cap.isOpened():
     ret, frame = cap.read()
     
+    frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
     if count == 0:
-        # Hard code color range for table and mask out everything else
-        hsv_img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        lower = np.array([90, 50, 50])
-        upper = np.array([120, 255, 255])
-        mask = cv.inRange(hsv_img, lower, upper)
-        table = cv.bitwise_and(frame, frame, mask = mask)
+        table = find_table(frame_hsv)
 
-        table_gray = cv.cvtColor(table, cv.COLOR_BGR2GRAY)
-        contours, hierarchy = cv.findContours(table_gray, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
-        # Find the largest contour as the border of the table
-        c = max(contours, key = cv.contourArea)
-        hull = cv.convexHull(c)
-        new_mask = np.zeros_like(frame)
-        img_new = cv.drawContours(new_mask, [hull], -1, (255, 255, 255), -1)
-
+    new_mask = np.zeros_like(frame)
+    img_new = cv.drawContours(new_mask, [table], -1, (255, 255, 255), -1)
     cropped = cv.bitwise_and(frame, img_new)
 
-    sensitivity = 70
+    lower = np.array([150, 120, 120])
+    upper = np.array([165, 255, 255])
+    mask = cv.inRange(frame_hsv, lower, upper)
+
+    lines = cv.HoughLinesP(mask, 1, np.pi/180, 40, None, 20, 0)
+    print("Line coordinates:", lines)
+    cue = 0
+    if lines is not None:
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            cue += l
+            cv.line(frame, (l[0], l[1]), (l[2], l[3]), (0,0,0), 2, cv.LINE_AA)
+        cue = cue / len(lines)
+        cue = np.round(cue).astype(int)
+
+    sensitivity = 80
     hsv_img = cv.cvtColor(cropped, cv.COLOR_BGR2HSV)
     lower = np.array([0, 0, 255 - sensitivity])
     upper = np.array([255, sensitivity, 255])
     mask = cv.inRange(hsv_img, lower, upper)
     new_img = cv.bitwise_and(cropped, cropped, mask = mask)
 
-    # Find balls
-    # Param2: higher = less circles
-    gray_img = cv.cvtColor(new_img, cv.COLOR_BGR2GRAY)
-    circles = cv.HoughCircles(gray_img, cv.HOUGH_GRADIENT, 1, 20, param1=10, param2=10, minRadius=7, maxRadius=13)
-    if circles.any():
-        circles = np.uint16(np.around(circles))
-        # print(circles)
-        # print(circles.shape)
-        for i in circles[0, :]:
-            cv.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            cv.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+    circles = cv.HoughCircles(mask, cv.HOUGH_GRADIENT, 1, 20, param1=11, param2=11, minRadius=10, maxRadius=15)
+    print("Ball coordinates:", circles)
+    if lines is not None:
+        center = np.array([640, 480])
+        d0 = np.linalg.norm(cue[0:2] - center)
+        d1 = np.linalg.norm(cue[2:4] - center)
+        if d0 < d1:
+            cue[0], cue[2] = cue[2], cue[0]
+            cue[1], cue[3] = cue[3], cue[1]
+        print("Cue stick coordinates:", cue)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            d0 = 1000
+            min = 0
+            for i in circles[0, :]:
+                d1 = np.linalg.norm(i[0:2] - cue[2:4])
+                if d1 < d0:
+                    d0 = d1
+                    min = i
+            print("Cue ball coordinates:", min)
+            cv.circle(frame, (min[0], min[1]), min[2], (0, 255, 0), 2)
+            cv.circle(frame, (min[0], min[1]), 2, (0, 0, 255), 3)
+            cv.line(frame, (cue[2], cue[3]), (min[0], min[1]), (255,255,0), 2, cv.LINE_AA)
 
-    # cv.imshow("images", np.hstack([image, output]))
     cv.imshow("frame", frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+    if cv.waitKey(1) == ord('q'):
+            cap.release()
+            cv.destroyAllWindows()
+            break
 
     count += 1
     if count >= 30:
